14e88d894644247cf6c1e9945072e723d8a7e2be
habibo-dev <habibolezar04@gmail.com>
2026-09-14 23:05:10 +0100

    feat: add loading state

---
+++ app/loading.tsx
@@ -0,0 +1,1 @@
+export default function Loading(){return <main className="page"><div className="container loading"><div className="loading-orb"/><p>Chargement de votre expérience voyage…</p></div></main>}
